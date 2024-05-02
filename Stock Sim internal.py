485b4a9faa772f9c4ea7b2ba6fc0dcb4de5debee
swifty4789 <teppei.usami@education.nsw.gov.au>
2024-05-02 11:43:54 +1000

    Recovered files
    Files recovered from previous computer factory reset.

---
+++ Stock Sim internal.py
@@ -0,0 +1,83 @@
+import datetime
+import random
+import time
+
+SL =   {'SKB': 1000000,
+        'FRT': 1000000,
+        'AMG': 1000000,
+        'DFT': 1000000,
+        'MOG': 100000,
+        'MEW': 100000,
+        'LMX': 100000,
+        'OHO': 100000,
+        'EDG': 10000,
+        'GNG': 10000,
+        'GYT': 10000,
+        'SGA': 10000,
+        'CNT': 1000,
+        'LGR': 1000,
+        'FNM': 1000,
+        'RIZ': 1000,
+        'DDN': 1000}
+
+wallet = 0
+stockamount = 0
+ListSL = []
+
+def randomprice():
+    now = datetime.datetime.now()
+    sec = int(now.strftime("%S"))
+    num = random.randint(1, 101)
+    roundednum = sec*num
+    roundednum = roundednum/100
+    roundednum = round(roundednum)
+
+def purchase():
+    buyscreen = 0
+    while buyscreen == 0:
+        a = input("Purchase stocks and amount:")
+        words = a.split()
+        
+        if len(words) == 2:
+            requestint = (words[1])
+            requeststr = (words[0])
+
+            if requestint.isdigit():
+                requestint = int(words[1])
+                requeststr = str(words[0])
+                    
+                if requeststr in SL:
+                    stockamount = SL[requeststr]
+                    
+                    if stockamount >= requestint:
+                        
+                        if stockamount >= requestint:
+                            stockamount = stockamount - requestint
+                            SL[requeststr] = SL[requeststr] - requestint
+                            buyscreen = buyscreen + 1
+                            time.sleep(2)
+                            
+                        else:
+                            print("Request Invalid")
+
+                    else:
+                        print("Request Invalid")
+
+                else:
+                    print("Request Invalid")
+
+            else:
+                print("Request Invalid")
+
+        else:
+            print("Request Invalid")
+
+
+input("Choose an action:\n")
+
+                
+
+
+
+
+
