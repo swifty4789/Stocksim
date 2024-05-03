14609dc297efcbfac57e8fd763b8d04f08fb0381
swifty4789 <teppei.usami@education.nsw.gov.au>
2024-05-03 12:29:26 +1000

    tweaks to internal programming

--- Stock Sim internal.py
+++ Stock Sim internal.py
@@ -19,44 +19,60 @@ SL =   {'SKB': 1000000,
         'FNM': 1000,
         'RIZ': 1000,
         'DDN': 1000}
+#prices
+#1000000 = 100/share
+#100000  = 250/share
+#10000   = 500/share
+#1000    = 1000/share
 
+#initial credits
 wallet = 1000
+datetime.datetime.now()
+     
 
-def randomprice():
+def price():
     now = datetime.datetime.now()
-    sec = int(now.strftime("%S"))
-    num = random.randint(1, 101)
-    roundednum = sec*num
-    roundednum = roundednum/100
-    roundednum = round(roundednum)
+    
 
+    
 def purchase():
-    buyscreen = 0
-    while buyscreen == 0:
-        stockamount = 0
-        ListSL = []
-        a = input("Please enter desired stocks and amount to purchase:")
-        words = a.split()
+    stockamount = 0
+    purchaseUI = 1
+    while purchaseUI == 1:
+        a = input("""Please enter desired stocks and amount to purchase (example: ABC 123)\n
+Type 'x' to return to menu: """)
+        if a in ['x']:
+            #replace with list of stocks and buttons to choose each share option
+            words = a.split()
         
-        if len(words) == 2:
-            requestint = (words[1])
-            requeststr = (words[0])
+            #Check if input from player is valid and correct syntax
+            if len(words) == 2:
+                requestint = (words[1])
+                requeststr = (words[0])
 
-            if requestint.isdigit():
-                requestint = int(words[1])
-                requeststr = str(words[0])
-                    
-                if requeststr in SL:
-                    stockamount = SL[requeststr]
-                    
-                    if stockamount >= requestint:
+                if requestint.isdigit():
+                    requestint = int(words[1])
+                    requeststr = str(words[0])
                         
-                        if stockamount >= requestint:
-                            stockamount = stockamount - requestint
-                            SL[requeststr] = SL[requeststr] - requestint
-                            buyscreen = buyscreen + 1
-                            time.sleep(2)
+                    if requeststr in SL:
+                        stockamount = SL[requeststr]
                             
+                        if stockamount >= requestint:
+                            confirm = input("Confirm purchase of "+ str(a))
+                            #confirm purchase
+
+                            if confirm in ['y','Y','Yes','yes','Confirm','confirm','c','C']:
+                               stockamount = stockamount - requestint
+                               SL[requeststr] = SL[requeststr] - requestint
+                               time.sleep(1)
+                               print('Purchase Successful')
+                               time.sleep(1)
+                               purchaseUI = purchaseUI - 1
+                               
+                            else:
+                               print('Transaction Cancelled')
+                               time.sleep(1)
+                                    
                         else:
                             print("Request Invalid")
 
@@ -69,17 +85,16 @@ def purchase():
             else:
                 print("Request Invalid")
 
-        else:
-            print("Request Invalid")
 
 while True:
     print("Current Balance:" + str(wallet))
     print("""Choose an action:
 1 . Purchase Shares
 2 . ---
-3 . ---""")
+3 . ---\n""")
     choice = input(':')
     if choice in ['1', 'Purchase Shares', 'purchase shares', 'Purchase shares']:
+        print(SL)
         purchase()
     
     else:
